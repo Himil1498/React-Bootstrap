8a170f22c0002f8a1d66f29d70bfc5613faafe3c
Himil Chauhan <himilchauhan1498@gmail.com>
2023-12-11 19:51:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './Component/Footer';
+import Paperbase from './Component/Paperbase';
+
+function App() {
+  return (
+    <div className="App">
+      <Paperbase/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
